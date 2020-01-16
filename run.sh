a932c5e26381aaefda5c9e0a5cfab64503b7e411
Danish <dasyed@umich.edu>
2020-01-15 21:00:25 -0500

    temp fix

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ cd code
 pip install http://download.pytorch.org/whl/cu90/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl
 pip install torchvision
 pip install nltk
+pip install future
 python main.py --cfg cfg/eval_coco_mod.yml --gpu 0
